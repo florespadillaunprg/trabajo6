da2eb501f588abdb2a73c017f0f37b3e15e6bdab
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-11-17 10:42:15 -0500

    se agrego la carpeta file(lote.bat)

---
+++ lote.bat
@@ -0,0 +1,127 @@
+python flores/dobles/ejercicio01.py "alex" 25 25 25 2 6 1.45
+python flores/dobles/ejercicio03.py "jose" 30 40 50 5 7 2
+python flores/dobles/ejercicio03.py "maria" 100 20 50 1 8 1.1
+
+python flores/dobles/ejercicio02.py "juan" "zuzuki" "hilux" "toyota" 500000 200000 300000
+python flores/dobles/ejercicio02.py "cristian" "honda" "mercury" "nissan" 96520 785623 21545
+python flores/dobles/ejercicio02.py "iker" "chevrolet" "volvo" "hyunday" 896451 785632 21534
+
+python flores/dobles/ejercicio03.py "eduardo" 025634545 "rambler" "mosati" "vilcotons" 120 50 30
+python flores/dobles/ejercicio03.py "ramon" 0256311564 "negro" "rosada" "azul" 80 45 21
+python flores/dobles/ejercicio03.py "messi" 0122455412 "verde" "celeste" "amarillo" 72 33.5 18.9
+
+python flores/dobles/ejercicio04.py "ronaldo" "25453265" 0125885432 "blancas" "negros" "verdes" 200 52.3 156.3
+python flores/dobles/ejercicio04.py "neymar" "54623589" 0741259852 "azules" "marrones" "rojos" 196.2 70.6 296.8
+python flores/dobles/ejercicio04.py "andres" "65476293" 0258320265 "naranjas" "dorados" "celestes" 326 83.3 98.9
+
+python flores/dobles/ejercicio05.py "nelson" "boeing 747" "boeing 777" "boeing 767" 50000000 60000000 99900000
+python flores/dobles/ejercicio05.py "junior" "boeing 757" "boeing 737" "aiburs a340" 88845102 2154856 33333100
+python flores/dobles/ejercicio05.py "sergio" "aiburs a330" "mcdonel 80" "aiburs a320" 8956402 5421784 95210020
+
+python flores/dobles/ejercicio06.py "esteban" "amarilla" "roja" "azul" 53.2 65.4 23.4
+python flores/dobles/ejercicio06.py "julia" "rosada" "morada" "celeste" 26 45 53.8
+python flores/dobles/ejercicio06.py "beto" "marron" "naranja" "blanca" 51 68 58
+
+python flores/dobles/ejercicio07.py "luis" "kalos" "avenue" "salvaje" 82 32 15
+python flores/dobles/ejercicio07.py "carlos" "rumeur" "fantasy" "million" 65 40 12.2
+python flores/dobles/ejercicio07.py "ronaldhino" "gio" "invictus" "bvlgary" 200 93.2 63.25
+
+python flores/dobles/ejercicio08.py "kelly" 20 26 25 5 10 20
+python flores/dobles/ejercicio08.py "salazar" 10 15 21 6 8 15
+python flores/dobles/ejercicio08.py "arturo" 12 13 14 8 16 30
+
+python flores/dobles/ejercicio09.py "john" 01245652 5 6 7 2 3 4
+python flores/dobles/ejercicio09.py "ivan" 01796532 10 12 14 6 9 8
+python flores/dobles/ejercicio09.py "antoine" 0125484521 9 6 13 7.2 14.1 3.65
+
+python flores/dobles/ejercicio10.py "clemente" 21435012 15 9 2 68 96 251
+python flores/dobles/ejercicio10.py "gerad" 0152565653 9 10 12 900 654 32
+python flores/dobles/ejercicio10.py "yordi" 012345653 5 6 7 54 645 753
+
+
+
+
+
+python flores/multiples/ejercicio01.py "david" 02659865321 "dodge" "chysler" "pontiac" 86522 87565 32586
+python flores/multiples/ejercicio01.py "rafael" 02154532232 "isuzu" "acura" "saturn" 612000 320100 520051
+python flores/multiples/ejercicio01.py "daniel" 01245865321 "jeep" "lexus" "buick" 986531 65554 78541
+
+python flores/multiples/ejercicio02.py "alfonso" 0124575321 "arden" "channel" "robanne" 96 23.3 8.96
+python flores/multiples/ejercicio02.py "marcelo" 0214588652 "dolce" "gabanna" "laurent" 230 123.2 56.5
+python flores/multiples/ejercicio02.py "james" 01245875412 "boss" "klein" "dior" 653 96.28 32.2
+
+python flores/multiples/ejercicio03.py "federico" 021545251 "roja" "azul" "morada" 65.2 54 52
+python flores/multiples/ejercicio03.py "ernesto" 0125854165 "celeste" "verde" "amarilla" 32.0 84.5 96.2
+python flores/multiples/ejercicio03.py "enrrique" 0458521232 "marron" "rosada" "negra" 54.2 39.6 46.9
+
+python flores/multiples/ejercicio04.py "mariano" 021541284  2 10 1 65 23 85
+python flores/multiples/ejercicio04.py "rodrigo" 0985241212 6 11 13 98 45 32.6
+python flores/multiples/ejercicio04.py "mario" 015852112 7 14 17 65.2 43.25 16.23
+
+python flores/multiples/ejercicio05.py "lucas" 02154552 5 6 7 5.5 6.7 9.8
+python flores/multiples/ejercicio05.py "pedro" 07956325 10 11 12 6.2 7.4 11.5
+python flores/multiples/ejercicio05.py "judas" 08523665 8 9 4 3.5 7.9 9.9
+
+python flores/multiples/ejercicio06.py "paolo" 0965365  2 3 2 6.5 9.8 16.5
+python flores/multiples/ejercicio06.py "edinson" 01564325 6 9 4 7.5 13.2 20
+python flores/multiples/ejercicio06.py "yoshimar" 09856132 7 2 5 6.3 4.2 19.9
+
+python flores/multiples/ejercicio07.py "miguel" 78593221 09865231 5 6 9 12.3
+python flores/multiples/ejercicio07.py "steven" 65321548 08532589 6 2 8 11.4
+python flores/multiples/ejercicio07.py "renato" 51324851 09561237 2 3 7.7 15.6
+
+python flores/multiples/ejercicio08.py "cristofer" 5 "venero" "terrel" "baldor" 30.2 25.2 18.5
+python flores/multiples/ejercicio08.py "josemir" 10 "lumbreras" "figueroa" "larson" 25.3 31.2 22.5
+python flores/multiples/ejercicio08.py "gabriel" 8 "demidovich" "thomas george" "hostetler" 20.5 42.1 33.3
+
+python flores/multiples/ejercicio09.py "raul" 71510215 0851202 "stanford" "loro" "justus" 5.3 4.8 2.8
+python flores/multiples/ejercicio09.py "timoteo" 65324589 09541265 "surco" "layconsa" "faber" 2.8 3.5 3.2
+python flores/multiples/ejercicio09.py "yuriel" 59454532 07523026 ""
+
+python flores/multiples/ejercicio10.py "sebastian" 65324825 6 5 4 1.2 1 13.2
+python flores/multiples/ejercicio10.py "kevin" 95926321 7 9 8 1.5 0.8 15.9
+python flores/multiples/ejercicio10.py "italo" 46272829 10 11 12 1.8 2 25.6
+
+
+
+
+
+python flores/simples/ejercicio01.py "adrian" 5 9.6
+python flores/simples/ejercicio01.py "aldo" 2 3.6
+python flores/simples/ejercicio01.py "ederson" 1 10.6
+
+python flores/simples/ejercicio02.py "alisson" "stanford" "venero" "de 200 hojas" 5.5 6.5 15.6
+python flores/simples/ejercicio02.py "claudio" "loro" "racso" "de 100 hojas" 4.5 20.63 8.65
+python flores/simples/ejercicio02.py "keylor" "surco" "lumbreras" "de 20 hojas" 2.5 15.9 2.3
+
+python flores/simples/ejercicio03.py "wilson" 5 6 12 13
+python flores/simples/ejercicio03.py "mohamed" 2 3 9.6 11.2
+python flores/simples/ejercicio03.py "firmino" 4 2 7.9 9.6
+
+python flores/simples/ejercicio04.py "angel" 2 "racso" "fisica" "calculo" 25.3 21.2 26.6
+python flores/simples/ejercicio04.py "alejandro" 3 "analisis" "geometria" "algebra" 30.2 32.1 28.9
+python flores/simples/ejercicio04.py "nicolas" 5 "quimica" "lumbreras" "trigonometria" 21.5 15.98 19.96
+
+python flores/simples/ejercicio05.py "godin" "huawei" "motorola" "lg" 562 262.6 358.6
+python flores/simples/ejercicio05.py "fernando" "nokia" "sansumg" "alcatel" 321.2 987.6 123.2
+python flores/simples/ejercicio05.py "darwin" "bitel" "claro" "movistar" 652.0 325.3 552.9
+
+python flores/simples/ejercicio06.py "alexis" 65872154 "lg" "samsung" "sony" 1253.6 239.5 654.6
+python flores/simples/ejercicio06.py "gary" 75421245 "motorola" "alcatel" "" 2365.4 356.2 623.2
+python flores/simples/ejercicio06.py "felipe" 32658954 "huawei" "sony" "lg" 3650.01 400.0 321.02
+
+python flores/simples/ejercicio07.py "alberto" 65324865 5 6 7 2.1 1.6 30.2
+python flores/simples/ejercicio07.py "ulises" 98525462 2 3 4 1.6 0.9 40.9
+python flores/simples/ejercicio07.py "pepe" 23265322 8 9 1 1.3 1.5 32.3
+
+python flores/simples/ejercicio08.py "ignacio" 075324563 5 6 8 20.5 3.06 9.04
+python flores/simples/ejercicio08.py "leao" 078845325 6 7 8 53.4 365.2 65.3
+python flores/simples/ejercicio08.py "aldair" 078946523 10 10 10 20 25 21.3
+
+python flores/simples/ejercicio09.py "oscar" 54653254 10 12 13 2.3 2.5 2.6
+python flores/simples/ejercicio09.py "sandi" 89215486 21 5 9 3.6 5.1 2.4
+python flores/simples/ejercicio09.py "paul" 21658978 2 3 1 3.2 2.9 3.3
+
+python flores/simples/ejercicio10.py "alan" 07845632 5 6 7 0.5 0.6 0.7
+python flores/simples/ejercicio10.py "valentin" 07854632 10 11 12 1 1.5 1.8
+python flores/simples/ejercicio10.py "ollanta" 07845632 15 20 25 2.5 2.1 1.3
